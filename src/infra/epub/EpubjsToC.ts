b4e5e40eb09dade8ea530ac668ebccece48cc28f
Dimitar Pavlov <dimitar@shadez.info>
2024-12-26 18:35:34 +0200

    Attempting some fuzzy matching of chapter URLs to identify a ToC item when a direct mapping is not found

--- src/infra/epub/EpubjsToC.ts
+++ src/infra/epub/EpubjsToC.ts
@@ -6,6 +6,7 @@ export default class EpubjsToC {
   #ns: NavItem[];
   #toc: ToC;
   #hrefToItemMapping: Map<string, ToCItem>;
+  #idToItemMapping: [string, ToCItem][];
   #itemsFlat: ToCItem[];
 
   constructor(ns: NavItem[]) {
@@ -20,7 +21,41 @@ export default class EpubjsToC {
   }
 
   getToCItem(url: string) {
-   return this.#hrefToItemMapping.get(url);
+    const i = this.#hrefToItemMapping.get(url);
+    if (i) return i;
+
+    return this.#matchUrlToId(url);
+  }
+
+  // attempt to match the url to an id, hope for a partial match
+  #matchUrlToId(url: string) {
+    const entry = this.#idToItemMapping.find(([id]) => url.includes(id))?.[1];
+    if (entry) return entry;
+
+    return this.#fuzzyMatchIdToUrl(url);
+  }
+
+  #fuzzyMatchIdToUrl(url: string) {
+    const numbersInUrl = url.match(/\d+/g);
+
+    if (numbersInUrl) {
+      // typicaly the first number is a chapter reference, so we try to match it against the id
+      const chapterRef = numbersInUrl[0];
+
+      const entry = this.#idToItemMapping.find(([id]) => {
+        const numbersInId = id.match(/\d+/g);
+        if (!numbersInId) return false;
+
+        // we try to reverse-match the chapter ID to the link
+        // it's a match if chapter id 36 is part of a link like "chapter_0036"
+        const regex = new RegExp(`(?<!\\d)(?:[^\\d]*0*)${numbersInId[0]}(?!\\d)`);
+        return regex.test(chapterRef);
+      });
+
+      if (entry) return entry[1];
+    }
+
+    return undefined;
   }
 
   percentageAtToCItemEnd(i: ToCItem) {
@@ -42,15 +77,18 @@ export default class EpubjsToC {
       items: this.#ns.map(this.#toTocItem),
     };
 
-    this.#itemsFlat = this.#flattenTocItems(this.#toc.items);
+    this.#itemsFlat = this.#flattenItems(this.#toc.items);
     this.#hrefToItemMapping = new Map(this.#itemsFlat.map(f => [f.link, f]));
+
+    const nsFlat = this.#flattenItems(this.#ns);
+    this.#idToItemMapping = nsFlat.map(n => [n.id, this.#toTocItem(n)]);
   }
 
-  #flattenTocItems(is: ToCItem[]) {
-    const result: ToCItem[] = [];
+  #flattenItems<T extends { subitems?: T[] }>(is: T[]) {
+    const result: T[] = [];
     is.forEach(i => {
       result.push(i)
-      if (i.subitems.length > 0) result.push(...this.#flattenTocItems(i.subitems));
+      if (i.subitems?.length > 0) result.push(...this.#flattenItems(i.subitems));
     });
 
     return result;
